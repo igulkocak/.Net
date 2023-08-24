c0338a0fa6740a80f743f0798d3339dd030b9210
igulkocak <80197917+igulkocak@users.noreply.github.com>
2023-08-24 20:43:12 +0300

    Add files via upload

---
+++ C#/Tekli Sayı/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TekliSayi
+{
+    class Program
+    {
+        //XOR işlemi 
+        static int TekliSayi(int[] sayilar)
+        {
+            int result = 0; //0'la hir sayı XOR'landığında kendisini verdiğinden resultı başta 0 yaptım
+            foreach (int c in sayilar) 
+            {
+                result ^= c; //Dizideki elemanları result ile XOR işlemine sokuyorum
+            }
+            return result;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            int[] sayi= { 5, 7, 8, 8, 5 };
+            int ts = TekliSayi(sayi); //TekliSayi fonksiyonuna sayi dizisini atıyorum, sayılar XOR'lanıyor ve tek olan sayı ts'ye atanıyor
+            Console.WriteLine("Tek Sayı: " + ts);
+        }
+    }
+}
+
+
+/* 0000 ^ 0101 = 0101 
+  0101^ 0111 = 0010 
+  0010^ 1000 = 1010
+  1010^ 1000 = 0010
+  0010^0101 = 0111 result=7 */
